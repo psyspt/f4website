463f77ac5773171762acf7faf7f379c754ba0885
truonggiang12134 <truonggiang12134@fc945598-3f59-6c2b-9670-01d974e6c612>
2010-11-24 04:05:41 +0000

    update trang xác nhận đăng ký

---
+++ source_working/WebPage_F4/WebPage_F4/XacNhanDangKy.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebPage_F4
+{
+    public partial class XacNhanDangKy : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //kiểm tra người dùng đã click vào link xác nhân chưa
+            if (Request.QueryString["username"] != null)
+            {
+                //lưu xuống csdl người dùng đã kích hoạt 
+            }
+        }
+    }
+}
